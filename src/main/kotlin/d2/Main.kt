972eb0aed3bdab89ec06308bb54c998b20ffec40
Cyprian Lenoir <c.lenoir@i2solutions.de>
2025-12-03 10:05:16 +0100

    day 2 mod

--- src/main/kotlin/d2/Main.kt
+++ src/main/kotlin/d2/Main.kt
@@ -4,71 +4,66 @@ import dev.cypdashuhn.aoc25.getLines
 import dev.cypdashuhn.aoc25.getLinesStream
 
 fun main() {
-    println(Id.doesIdRepeat("12"))
-    println(Id.doesIdRepeat("101"))
-    println(Id.doesIdRepeat("302"))
-    println(Id.doesIdRepeat("330"))
-    println(Id.doesIdRepeat("1213"))
-
+    calc1()
     println("---")
+    calc2()
+}
 
-    println(Id.doesIdRepeat("12"))
-    println(Id.doesIdRepeat("11"))
-    println(Id.doesIdRepeat("1212"))
-    println(Id.doesIdRepeat("123123"))
-    println(Id.doesIdRepeat("12341234"))
+fun calc1() {
+    val regex = """^([0-9]+)\1{1}$""".toRegex()
+    val id = getIds().sumOf { it.sumOfMatchingIds(regex) }
+    println(id)
+}
+
+fun calc2() {
+    val regex = """^([0-9]+)\1+$""".toRegex()
+    val id = getIds().sumOf { it.sumOfMatchingIds(regex) }
+    println(id)
 }
 
 fun getIds(): List<Id> {
     val lines = getLines(2, "input")
     assert(lines.size == 1) { "should be just one" }
+
     val first = lines[0]
-    val ids = first.split(",").map(Id::fromString)
+    val ids = first
+        .split(",")
+        .map(Id::fromString)
     return ids
 }
 
-// ([0-9])\1+
 data class Id(
-    val first: String,
-    val second: String
+    val first: Long,
+    val second: Long,
+    val range: LongRange
 ) {
     companion object {
         fun fromString(s: String): Id {
             val parts = s.split("-")
             assert(parts.size == 2) { "Must be size of 2!" }
 
-            return Id(parts[0], parts[1])
+            val first = parts[0].toLong()
+            val second = parts[1].toLong()
+            val range = first..second
+
+            return Id(first, second, range)
         }
 
-        fun doesIdRepeat(id: String): Boolean {
-            var pattern = ""
-            var localMatch = ""
-            var matches = 1
-
-            for ((idx, char) in id.withIndex()) {
-                if (idx == 0) {
-                    pattern = char.toString()
-                    continue
-                }
-
-                var currentTake = localMatch + char
-                if (currentTake == pattern.take(localMatch.length + 1)) {
-                    localMatch += char
-                } else {
-                    if (char == pattern[0]) {
-                        localMatch = ""
-                        localMatch += char
-                        matches += 1
-                    } else if (matches == 1){
-                         pattern += char
-                         localMatch += char
-                    } else {
-                        return false
-                    }
-                }
-            }
-            return matches > 1
+        fun doesIdRepeat(id: String, regex: Regex): Boolean {
+            val matches = regex.containsMatchIn(id)
+            return matches
         }
     }
 
+    fun allMatchingIds(regex: Regex): List<Long> {
+        return range
+            .map(Long::toString)
+            .filter { doesIdRepeat(it, regex) }
+            .map(String::toLong)
+    }
+
+    fun sumOfMatchingIds(regex: Regex): Long {
+        val ids = allMatchingIds(regex)
+        return ids.sum()
+    }
 }
